3fe5be02529d27990ddfc36458b5c765342a55b0
Mudassar Ali <mudassar174@gmail.com>
2025-09-21 00:44:07 -0700

    update message

---
+++ lib/homescren.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Homescren extends StatefulWidget {
+  const Homescren({super.key});
+
+  @override
+  State<Homescren> createState() => _HomescrenState();
+}
+
+class _HomescrenState extends State<Homescren> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
